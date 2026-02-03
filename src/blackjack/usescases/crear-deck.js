2e3956982ba8c3b85a9df09c08284875edf7dc11
cpr.dev <necroyl86@gmail.com>
2026-02-03 14:33:59 +0100

    First commit

---
+++ src/blackjack/usescases/crear-deck.js
@@ -0,0 +1,33 @@
+// Esta función crea un nuevo deck
+import _ from 'underscore';
+
+/**
+ * 
+ * @param {array} tiposDeCarta 
+ * @param {array} tiposEspeciales 
+ * @returns {array} retorna un nuevo deck de cartas
+ */
+export const crearDeck = (tiposDeCarta, tiposEspeciales) => {
+    if ( !tiposDeCarta || tiposDeCarta.length === 0 ) 
+        throw new Error('TiposDeCarta es obligatorio como un arreglo de string');
+
+    if ( !tiposEspeciales || tiposEspeciales.length === 0 ) 
+        throw new Error('tiposEspeciales es obligatorio como un arreglo de string');
+
+    let deck = [];
+
+    for( let i = 2; i <= 10; i++ ) {
+        for( let tipo of tiposDeCarta ) {
+            deck.push( i + tipo);
+        }
+    }
+
+    for( let tipo of tiposDeCarta ) {
+        for( let esp of tiposEspeciales ) {
+            deck.push( esp + tipo);
+        }
+    }
+    
+    deck = _.shuffle( deck );
+    return deck;
+}
---
+++ src/blackjack/usescases/valor-carta.js
@@ -0,0 +1,11 @@
+/**
+ * Retorna el valor de la carta
+ * @param {*} carta 
+ * @returns {number}
+ */
+export const valorCarta = ( carta ) => {
+    const valor = carta.substring(0, carta.length - 1);
+    return ( isNaN( valor ) ) ? 
+            ( valor === 'A' ) ? 11 : 10
+            : valor * 1;
+}
